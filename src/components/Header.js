98915d99f2dae794c2705ee2e89d01bc369aed37
Ramazan <ramazanmyktarbekuulu@gmail.com>
2023-01-09 18:54:23 +0600

    save mesage

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button>add todo</button>
+    </div>
+  );
+};
+
+export default Header;
