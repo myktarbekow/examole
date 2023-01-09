af2306901b9a95c79171fb21ed2c0817f9ff3d41
Ramazan <ramazanmyktarbekuulu@gmail.com>
2023-01-09 18:56:31 +0600

    save footer

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <input type="text" />
+      <button>add todo</button>
+    </div>
+  );
+};
+
+export default Footer;
